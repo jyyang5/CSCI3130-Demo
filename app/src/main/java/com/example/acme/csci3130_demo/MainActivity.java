12a778a8faa895cc017d0dae0c4e359f5f5a2afc
jyyang5 <jyyang55@outlook.com>
2018-01-19 20:39:20 -0400

    Assignment 1
    Add a button and a text field to the UI. The user should input text on the field
    and click the button after. The text from the field should then be copied to the textbox that was already in the layout.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.acme.csci3130_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /* Called when the user tap the Send button */
+    public void sendMessage(View view){
+        // In response to the tap
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString(); // convert message to string
+        intent.putExtra(EXTRA_MESSAGE, message);      // put the value in EditText to intent
+        startActivity(intent);                          // create
+    }
+
+
 }
